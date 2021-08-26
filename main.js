006fab929b0ea40dfd2b36eef185447a9a426944
KamilaMoura1 <kamila.demoura93@gmail.com>
2021-08-26 18:25:51 -0400

    Updates

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 import data from './data/pokemon/pokemon.js';
 
 import {
-  filtrarPelaGeracao,
+  // filtrarPelaGeracao,
   ordenarPorNum,
   ordenarPorNome,
   filterType,
   sortCp,
   typeStats,
   filterName,
-  sortFilter
+  // sortFilter
 } from './data.js';
 
 const pokemonList = data.pokemon;
@@ -21,21 +21,21 @@ const displayPokes = (pokemonData) => {
   allPokemonCards = pokemonData.map((elem) => {
 
     let props = elem["special-attack"]
-
     let attName = props.map(function (specialAttack) {
       return specialAttack["name"]
-
     })
-
     let attType = props.map(function (typeAttack) {
       return typeAttack["type"]
-
     })
 
-      return allPokemonCards = `<div class="card" id="card">
+    pokeCard.insertAdjacentHTML("beforeend", `
+      <div class='cardBtnContainer'>
+      <div class="card" id="card" data-poke=${pokemonList.indexOf(elem) + 1}>
         <div class="gridContainerUp" id="gridContainerUp"> 
-          <div class="title"> ${elem.name.toUpperCase()}</div>
-          <div class="number">#${elem.num}</div>
+          <div class="mainInfo">
+            <div class="title"> ${elem.name.toUpperCase()}</div>
+            <div class="number">#${elem.num}</div>
+          </div>
           <div class="sideInfo">
             <div class="maxHp">${elem.stats["max-hp"]}HP</div>
             <div class="maxCp">${elem.stats["max-cp"]}CP</div>
@@ -65,33 +65,138 @@ const displayPokes = (pokemonData) => {
           </div>
       </div>
     </div>
-`;
-
+`);
   });
-  pokeCard.innerHTML = allPokemonCards.join("");
 };
 displayPokes(pokemonList);
+
+const battlePage = (randomPoke, chosenPoke) => {
+  const randomPokeCP = parseInt(randomPoke.stats["max-cp"])
+  const chosenPokeCP = parseInt(chosenPoke.stats["max-cp"])
+  const container = document.createElement('div');
+  container.className = 'battleContainer';
+  const template = `
+  <div class="text-box">
+  
+    <div class='poke1 poke' id="chosenPoke">
+      <div class='poke1Info info'>
+        <p>${chosenPoke.name.toUpperCase()}</p>
+      </div>
+      <div>
+        <img class="poke1Photo photo" src=" ${chosenPoke.img}"></img>
+      </div>
+    </div>
+    <div class='poke2 poke' id="randomPoke">
+      <div class='poke2Info info' >
+        <p>${randomPoke.name.toUpperCase()}</p>
+      </div>
+      <div>
+        <img class="poke2Photo photo" src=" ${randomPoke.img}"></img>
+      </div>
+    </div>
+    <section class="dialogue-box">
+      <img class="box" src="./assets/dialogue-box.png">
+      <p class='message'>Um Pokemon selvagem apareceu...</p>
+    </section>
+    <div id="winner-box" class="box-content" style="display: none">
+    </div>
+    <div class="draw-box" style="display: none"></div>
+
+`
+  container.innerHTML = template
+
+  const dialogueBox = container.querySelector('.dialogue-box')
+  const winnerBox = container.querySelector('#winner-box')
+
+
+  if (randomPokeCP > chosenPokeCP) {
+    dialogueBox.addEventListener('click', () => {
+      const loserPoke = container.querySelector('#chosenPoke')
+      const winnerPoke = container.querySelector('#randomPoke')
+
+      loserPoke.style.display = 'none';
+      winnerPoke.classList.add('winner-random');
+      
+      winnerBox.innerHTML += `
+      <p>${randomPoke.name.toUpperCase()} VENCEU!</p> 
+      <p>MAX-CP: ${randomPokeCP}</p>
+      `
+      winnerBox.style.display = 'block'
+    })
+
+  } else if (randomPokeCP < chosenPokeCP) {
+    dialogueBox.addEventListener('click', () => {
+      const loserPoke = container.querySelector('#randomPoke')
+      const winnerPoke = container.querySelector('#chosenPoke')
+      loserPoke.style.display = 'none';
+      winnerPoke.classList.add('winner-chosen')
+      winnerBox.style.display = 'block'
+      winnerBox.innerHTML += `
+      <p>${chosenPoke.name.toUpperCase()} VENCEU!</p> 
+      <p>MAX-CP: ${chosenPokeCP}</p>
+      `
+    })
+  } else {
+    const randomPoke = container.querySelector('#randomPoke')
+    const chosenPoke = container.querySelector('#chosenPoke')
+    winnerBox.innerHTML += `
+      <p>EMPATE!</p> 
+      <p>${randomPokeCP} VS ${chosenPokeCP}</p>
+      `
+  }
+
+
+  return container
+}
+
+
+const renderPage = (randomPoke, chosenPoke) => {
+  const main = document.getElementById('root');
+  main.innerHTML = '';
+  main.appendChild(battlePage(randomPoke, chosenPoke))
+}
+
+const getUserPoke = (pokemonList) => {
+  const getPoke = document.querySelectorAll("[data-poke]")
+  for (let poke of getPoke) {
+    poke.addEventListener('click', (e) => {
+      const target = e.target;
+      const dataIndex = target.parentNode.parentNode.parentNode
+      const pokeIndex = dataIndex.dataset.poke
+      const chosenPoke = pokemonList[pokeIndex - 1]
+      const randomIndex = Math.floor(Math.random() * 251)
+      const randomPoke = pokemonList[randomIndex]
+      //const battleField = document.querySelector('#battle-field')
+      //battleField.classList.toggle("show")
+      console.log(chosenPoke)
+      console.log(randomPoke)
+
+      renderPage(randomPoke, chosenPoke)
+    })
+  }
+}
+
+getUserPoke(pokemonList)
 
 const field = document.getElementById("search")
 let autoCompleteValues;
 
-field.addEventListener("input", ({target}) =>{
+field.addEventListener("input", ({ target }) => {
   const fieldContent = target.value.toLowerCase()
   let filterPokes = document.getElementById("card-container")
-   
-  if(fieldContent.length){
+
+  if (fieldContent.length) {
     filterPokes.innerHTML = ''
     autoCompleteValues = filterName(pokemonList, fieldContent)
-    sortFilter(autoCompleteValues)
     displayPokes(autoCompleteValues)
+    getUserPoke(pokemonList)
 
-  } else{
+  } else {
     filterPokes.innerHTML = ''
     displayPokes(pokemonList)
+    getUserPoke(pokemonList)
   }
-  
 });
-
 
 let selecionarPorTipo;
 const filtrar = document.getElementById("tipoPokemon");
@@ -101,7 +206,7 @@ filtrar.addEventListener('change', () => {
   selecionarPorTipo = filtrar.value;
 
   displayPokes(filterType(pokemonList, selecionarPorTipo));
-
+  getUserPoke(pokemonList)
 });
 
 let ordenarMaxCp;
@@ -113,6 +218,7 @@ ordenarPorCP.addEventListener('change', () => {
 
   sortCp(pokemonList, ordenarMaxCp)
   displayPokes(pokemonList)
+  getUserPoke(pokemonList)
 })
 
 let ordernarPorNumeros;
@@ -124,7 +230,7 @@ ordenar.addEventListener('change', () => {
 
   ordenarPorNum(pokemonList, ordernarPorNumeros);
   displayPokes(pokemonList);
-
+  getUserPoke(pokemonList)
 });
 
 
@@ -137,62 +243,56 @@ ordenarNomes.addEventListener('change', () => {
 
   ordenarPorNome(pokemonList, ordenarPorNomes);
   displayPokes(pokemonList);
-
+  getUserPoke(pokemonList)
 });
 
-let allPokemonGen;
+/*let allPokemonGen;
 const pokemonListGeneration = data.pokemon;
 const displayPokesGeneration = (pokemonData) => {
-
-
   const pokeCardGeneration = document.getElementById('slider');
-  
-
   allPokemonGen = pokemonData.map((elem) => {
     return `
   
        <div>
          <img class="pictures" src=" ${elem.img}"></img>
        </div>
-
        <div class="name-geracao">${elem.generation.num}
-
-
        </div> 
       `;
   });
-
   pokeCardGeneration.innerHTML = allPokemonGen.join("")
 };
-
 displayPokesGeneration(pokemonListGeneration);
-
-
 let ordernarPorGeracao;
 const ordenarGeracao = document.getElementById('ordemGeracao');
 ordenarGeracao.addEventListener('change', () => {
   const getpokes = document.getElementById('slider');
   getpokes.innerHTML = '';
   ordernarPorGeracao = ordenarGeracao.value;
-
-
   displayPokesGeneration(filtrarPelaGeracao(pokemonListGeneration, ordernarPorGeracao));
-
 });
-
-
+*/
 const nextEl = document.getElementById('next');
 const previousEl = document.getElementById('previous');
-const sliderEl = document.getElementById('slider')
+const sliderEl = document.getElementById('card-container');
+const imgWidth = sliderEl.offsetWidth;
+var scrollPerClick = 0;
+var scrollAmount = 0;
 
 function onNextClick() {
-
-  const imgWidth = sliderEl.offsetWidth;
   sliderEl.scrollLeft += imgWidth;
 }
 
+
 function onPreviousClick() {
-  const imgWidth = sliderEl.offsetWidth;
+  /*sliderEl.scrollTo({
+    top:0,
+    left: (scrollAmount -= imgWidth),
+    behavior: 'smooth'
+  });
+  if(scrollAmount < 0) {
+    scrollAmount = 0;
+  }*/
   sliderEl.scrollLeft -= imgWidth;
 }
 
@@ -207,50 +307,3 @@ typeCalc.addEventListener('change', () => {
   filterpokes.innerHTML = `${typeStats(pokemonList, calcType)}% dos Pokemons são do tipo selecionado`;
 
 })
-
-
-/*
-
- const estatisticas = data.pokemon;
-
-
-
-  let baseAttack = estatisticas.map(baseAttack => baseAttack.stats['base-attack']);
-  let minAttack = baseAttack.reduce((a, b) => Math.min(a, b));
-  let maxAttack = baseAttack.reduce((a, b) => Math.max(a, b));
-  //let somAttack = baseAttack.reduce((a, b) => a + b);
- // let mediaAttack = parseInt(somAttack / baseAttack.length);
-  const table = document.getElementById("table");
-
-
- 
-  
-
-  
-
-  table.innerHTML = ` 
-  <table class=" box-alignment text-color table" >
-  <tr>
-    <th></th>
-    <th>Attack</th>
-    <th>Defense</th>
-    
-  </tr>
-  <tr>
-    <th>Minimo</th>
-    <th class="color-table-info">${minAttack}</th>
-    
-    
-  </tr>
-  <tr>
-  <th>Maximo</th>
-
-    <th class="color-table-info">${maxAttack}</th>
-    
-  </tr>
-  <tr>
-    
-</table>  
-
-  `
-*/
